7586f871d8094a760099eaf9b7f3f95e98bbb3f0
BusquetsLA <busquetsla@gmail.com>
2021-10-05 19:19:25 -0300

    added screenOptions and data transfer between screens

--- routes/MyStack.js
+++ routes/MyStack.js
@@ -8,9 +8,25 @@ const Stack = createStackNavigator();
 
 const MyStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name='Home' component={Home} />
-      <Stack.Screen name='Task' component={Task} />
+    <Stack.Navigator
+      screenOptions={{ // the general styles for the header on both screens
+        headerStyle: {
+          backgroundColor: 'red',
+        },
+        headerTintColor: 'white',
+        headerTitleStyle: 'bold',
+      }}>
+      <Stack.Screen
+        name='Home'
+        component={Home}
+      />
+      <Stack.Screen
+        name='Task'
+        component={Task}
+        options={
+          ({ route }) => {({ title: route.params.task })}
+        } // so the title of the task will appear at the header
+      />
     </Stack.Navigator>
   );
 };
