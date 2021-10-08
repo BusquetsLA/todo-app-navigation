60525e7bff4ae580210b9706c9cf43da91c80284
BusquetsLA <busquetsla@gmail.com>
2021-10-08 20:54:31 -0300

    added MyTab component to MyDrawer as the initial screen

--- routes/MyDrawer.js
+++ routes/MyDrawer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { View, Text } from 'react-native';
-import { AboutStack, HomeStack } from './MyStack';
+import { AboutStack } from './MyStack';
+import MyTab from './MyTab';
 
 const MyDrawer = () => {
   const Drawer = createDrawerNavigator();
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name='Home' component={HomeStack}/>
+      <Drawer.Screen name='Home' component={MyTab}/>
       <Drawer.Screen name='About' component={AboutStack}/>
     </Drawer.Navigator>
   );
