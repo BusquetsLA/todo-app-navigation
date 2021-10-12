26d1de91f92010e3b75c37084ef8eb7ba8232120
BusquetsLA <busquetsla@gmail.com>
2021-10-11 22:50:22 -0300

    implemented redux store

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { global } from './styles/global';
 import MyDrawer from './routes/MyDrawer';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
-    <NavigationContainer style={global.container}>
-      <MyDrawer />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer style={global.container}>
+        <MyDrawer />
+      </NavigationContainer>
+    </Provider>
   );
 };
