56dbdbde531674d5bf676c7df29293a64ad28c06
BusquetsLA <busquetsla@gmail.com>
2021-10-11 22:32:38 -0300

    delete and done now recieve an id as payload

--- redux/taskActions.js
+++ redux/taskActions.js
@@ -1,14 +1,14 @@
-import { ADD_TASK, DELETE_TASK, DONE_TASK } from "./taskTypes";
+import { ADD_TASK, DELETE_TASK, DONE_TASK } from './taskTypes';
 
 export const addTask = task => ({
   type: ADD_TASK,
   payload: task
 })
-export const deleteTask = task => ({
+export const deleteTask = id => ({
   type: DELETE_TASK,
-  payload: task
+  payload: id
 })
-export const doneTask = task => ({
+export const doneTask = id => ({
   type: DONE_TASK,
-  payload: task
+  payload: id
 })
