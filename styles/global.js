641df121ea99bb179463a0c8535aad2109ca1a55
BusquetsLA <busquetsla@gmail.com>
2021-10-12 15:55:58 -0300

    added task styles to global

--- styles/global.js
+++ styles/global.js
@@ -3,4 +3,14 @@ export const global = StyleSheet.create({
   container: {
     padding: 30,
   },
+  item: { // task styles
+    paddingTop: 15,
+    borderColor: 'black',
+    borderRadius: 5,
+    borderWidth: 1,
+    marginTop: 15,
+    borderStyle: 'dashed',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 });
